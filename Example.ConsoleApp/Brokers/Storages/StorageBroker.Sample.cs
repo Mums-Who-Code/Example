2d549409b1eabce709622eb9aa13860c38d591d2
Shri Humrudha <shrija@microsoft.com>
2021-11-24 12:17:14 -0800

    BROKER: Insert Sample

---
+++ Example.ConsoleApp/Brokers/Storages/StorageBroker.Sample.cs
@@ -0,0 +1,21 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using System.Collections.Generic;
+using Example.ConsoleApp.Models.Samples;
+
+namespace Example.ConsoleApp.Brokers.Storages
+{
+    internal partial class StorageBroker : IStorageBroker
+    {
+        List<Sample> Samples = new List<Sample>();
+
+        public Sample InsertSample(Sample sample)
+        {
+            Samples.Add(sample);
+
+            return sample;
+        }
+    }
+}
