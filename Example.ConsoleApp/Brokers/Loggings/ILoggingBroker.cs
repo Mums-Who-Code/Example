98675e0160b5c65224c5b9a4070d36467868fb31
Shri Humrudha <shrija@microsoft.com>
2022-01-17 08:19:55 -0800

    BROKER: Logging Broker

---
+++ Example.ConsoleApp/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,13 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using System;
+
+namespace Example.ConsoleApp.Brokers.Loggings
+{
+    internal interface ILoggingBroker
+    {
+        void LogError(Exception exception);
+    }
+}
