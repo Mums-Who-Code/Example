9540bd41a1ae55a84557b87be81f5038382c7543
Shri Humrudha Jagathisun <shri.jsh@outlook.com>
2022-02-23 19:10:50 -0800

    ShouldThrowServiceExceptionOnAddIfServiceErrorOccursAndLogIt -> FAIL

---
+++ Example.ConsoleApp/Models/Samples/Exceptions/SampleServiceException.cs
@@ -0,0 +1,16 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using Xeptions;
+
+namespace Example.ConsoleApp.Models.Samples.Exceptions
+{
+    public class SampleServiceException : Xeption
+    {
+        public SampleServiceException(Xeption innerException)
+            : base(message: "Sample serivce error occurred, contact support.",
+                  innerException)
+        { }
+    }
+}
