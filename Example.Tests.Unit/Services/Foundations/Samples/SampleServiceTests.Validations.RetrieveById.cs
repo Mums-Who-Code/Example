05f89a0963a917dd0e8af567df99597e349d77a1
Shri Humrudha Jagathisun <shri.jsh@outlook.com>
2022-03-26 16:41:05 -0700

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogIt -> FAIL

---
+++ Example.Tests.Unit/Services/Foundations/Samples/SampleServiceTests.Validations.RetrieveById.cs
@@ -0,0 +1,49 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using System;
+using Example.ConsoleApp.Models.Samples;
+using Example.ConsoleApp.Models.Samples.Exceptions;
+using Moq;
+using Xunit;
+
+namespace Example.Tests.Unit.Services.Foundations.Samples
+{
+    public partial class SampleServiceTests
+    {
+        [Fact]
+        public void ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogIt()
+        {
+            // given
+            var invalidSample = new Sample();
+            var invalidSampleException = new InvalidSampleException();
+
+            invalidSampleException.AddData(
+                key: nameof(Sample.Id),
+                values: "Id is required");
+
+            var expectedSampleValidationException =
+                new SampleValidationException(invalidSampleException);
+
+            // when
+            Action retrieveSampleByIdAction = () =>
+                this.sampleService.RetrieveSampleById(invalidSample.Id);
+
+            // then
+            Assert.Throws<SampleValidationException>(retrieveSampleByIdAction);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedSampleValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectSampleById(It.IsAny<int>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
